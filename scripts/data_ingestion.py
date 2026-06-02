5e7fc1242f49696c0231c2324c791c6a5a3e15c2
PrethivrajGS <prithivi2303@gmail.com>
2026-06-02 12:07:30 +0530

    Day 1: Data ingestion complete

---
+++ scripts/data_ingestion.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import pandas as pd
+
+DATA_PATH = Path("../data/raw")
+
+csv_files = list(DATA_PATH.glob("*.csv"))
+
+print(f"Total CSV Files Found: {len(csv_files)}")
+
+for file in csv_files:
+    print("\n" + "="*80)
+    print(f"FILE: {file.name}")
+
+    try:
+        df = pd.read_csv(file)
+
+        print("\nShape:")
+        print(df.shape)
+
+        print("\nColumns:")
+        print(df.columns.tolist())
+
+        print("\nData Types:")
+        print(df.dtypes)
+
+        print("\nFirst 5 Rows:")
+        print(df.head())
+
+        print("\nMissing Values:")
+        print(df.isnull().sum())
+
+        print("\nDuplicate Rows:")
+        print(df.duplicated().sum())
+
+    except Exception as e:
+        print(f"Error reading {file.name}: {e}")
---
+++ scripts/live_nav_fetch.py
@@ -0,0 +1,30 @@
+import requests
+import pandas as pd
+from pathlib import Path
+
+funds = {
+    "SBI_Bluechip": 119551,
+    "ICICI_Bluechip": 120503,
+    "Nippon_LargeCap": 118632,
+    "Axis_Bluechip": 119092,
+    "Kotak_Bluechip": 120841
+}
+
+save_path = Path("../data/raw/live_nav")
+save_path.mkdir(exist_ok=True)
+
+for fund_name, scheme_code in funds.items():
+
+    url = f"https://api.mfapi.in/mf/{scheme_code}"
+
+    response = requests.get(url)
+
+    data = response.json()
+
+    df = pd.DataFrame(data["data"])
+
+    file_name = save_path / f"{fund_name}.csv"
+
+    df.to_csv(file_name, index=False)
+
+    print(f"Saved: {file_name}")
